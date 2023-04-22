e2aea747f7503eaf28cde5eaa9e30dd3a430d69b
chiranjeevi3398 <gchiranjeevi3398@gmail.com>
2023-04-22 18:20:14 +0530

    Uber Assignment

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -29,11 +29,14 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public void register(Customer customer) {
+		customerRepository2.save(customer);
 		//Save the customer in database
 	}
 
 	@Override
 	public void deleteCustomer(Integer customerId) {
+		Customer customer = customerRepository2.findById(customerId).get();
+		customerRepository2.delete(customer);
 		// Delete customer without using deleteById function
 
 	}
@@ -43,10 +46,47 @@ public class CustomerServiceImpl implements CustomerService {
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
 
+		List<Driver> drivers = driverRepository2.findAll();
+		Driver bookedriver = null;
+		for(Driver driver : drivers){
+			if(driver.getCab().isAvailable()){
+				if(bookedriver == null || bookedriver.getDriverId() > driver.getDriverId()){
+					bookedriver = driver;
+				}
+			}
+		}
+		if(bookedriver == null)
+			throw new Exception("No cab available!");
+
+		TripBooking tripBooking = new TripBooking();
+		Customer customer = customerRepository2.findById(customerId).get();
+		tripBooking.setCustomer(customer);
+		tripBooking.setFromLocation(fromLocation);
+		tripBooking.setToLocation(toLocation);
+		tripBooking.setDistanceInKm(distanceInKm);
+		tripBooking.setBill(bookedriver.getCab().getPerKmRate() * distanceInKm);
+		tripBooking.setStatus(TripStatus.CONFIRMED);
+		tripBooking.setDriver(bookedriver);
+
+		bookedriver.getTripBookingList().add(tripBooking);
+		customer.getTripBookingList().add(tripBooking);
+
+		driverRepository2.save(bookedriver);
+
+		customerRepository2.save(customer);
+		//tripBookingRepository.save(tripBooking); ab iski zrorat nhi hai as this is the child.
+		return tripBooking;
+
+
 	}
 
 	@Override
 	public void cancelTrip(Integer tripId){
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setStatus(TripStatus.CANCELED);
+		tripBooking.setBill(0);
+		tripBooking.getDriver().getCab().setAvailable(true);
+		tripBookingRepository2.save(tripBooking);
 		//Cancel the trip having given trip Id and update TripBooking attributes accordingly
 
 	}
@@ -54,6 +94,11 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void completeTrip(Integer tripId){
 		//Complete the trip having given trip Id and update TripBooking attributes accordingly
+		TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
+		tripBooking.setStatus(TripStatus.COMPLETED);
+//		tripBooking.setBill(tripBooking.getDriver().getCab().getPerKmRate() * );
+		tripBooking.getDriver().getCab().setAvailable(true);
+		tripBookingRepository2.save(tripBooking);
 
 	}
 }
